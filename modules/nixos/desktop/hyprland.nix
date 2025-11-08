b3634139fcce08d06562ae5a30fb7e2ff438250b
vanadium5000 <vanadium5000@gmail.com>
2025-11-08 21:35:20 +0000

    hyprland fixes

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -53,17 +53,17 @@
           #border_part_of_window = true;
           layout = "dwindle"; # or master
 
-          "col.active_border" = lib.mkForce "rgb(${colorsNoHash.border-color})";
-          "col.inactive_border" = lib.mkForce "rgb(${colorsNoHash.border-color-inactive})";
+          "col.active_border" = "rgb(${colorsNoHash.border-color})";
+          "col.inactive_border" = "rgb(${colorsNoHash.border-color-inactive})";
         };
 
         group = {
           groupbar = {
-            "col.active" = lib.mkForce "rgb(${colorsNoHash.border-color})";
-            "col.inactive" = lib.mkForce "rgb(${colorsNoHash.border-color-inactive})";
+            "col.active" = "rgb(${colorsNoHash.border-color})";
+            "col.inactive" = "rgb(${colorsNoHash.border-color-inactive})";
           };
-          "col.border_active" = lib.mkForce "rgb(${colorsNoHash.border-color})";
-          "col.border_inactive" = lib.mkForce "rgb(${colorsNoHash.border-color-inactive})";
+          "col.border_active" = "rgb(${colorsNoHash.border-color})";
+          "col.border_inactive" = "rgb(${colorsNoHash.border-color-inactive})";
         };
 
         #-------------------------------------------------------------
@@ -202,20 +202,20 @@
         };
 
         # Animations
-        animations = {
-          enabled = true;
+        # animations = {
+        #   enabled = true;
 
-          # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-          bezier = "myBezier, 0.25, 0.9, 0.1, 1.02";
-          animation = [
-            "windows, 1, 7, myBezier"
-            "windowsOut, 1, 7, default, popin 80%"
-            "border, 1, 10, default"
-            "borderangle, 1, 8, default"
-            "fade, 1, 7, default"
-            "workspaces, 1, 3, myBezier, fade"
-          ];
-        };
+        #   # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+        #   bezier = "myBezier, 0.25, 0.9, 0.1, 1.02";
+        #   animation = [
+        #     "windows, 1, 7, myBezier"
+        #     "windowsOut, 1, 7, default, popin 80%"
+        #     "border, 1, 10, default"
+        #     "borderangle, 1, 8, default"
+        #     "fade, 1, 7, default"
+        #     "workspaces, 1, 3, myBezier, fade"
+        #   ];
+        # };
 
         bind = [
           "${mod},RETURN, exec, ${getExe terminal}"
