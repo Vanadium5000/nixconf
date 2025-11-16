05b33b2b5accab26a63a194251ebe9e2de379b25
Vanadium5000 <vanadium5000@gmail.com>
2025-11-16 21:07:35 +0000

    legion5i fix

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -64,7 +64,7 @@
         adwaita-icon-theme
       ])
       # GPU monitoring
-      ++ (lib.optional config.nixpkgs.config.cudaSupport [ pkgs.nvtopPackages.full ])
+      ++ (lib.optional config.nixpkgs.config.cudaSupport pkgs.nvtopPackages.full)
       # Custom desktop packages
       ++ (with self.packages.${pkgs.stdenv.hostPlatform.system}; [
         nwg-dock-hyprland
