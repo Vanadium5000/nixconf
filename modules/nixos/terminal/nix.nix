a3fcd1e894257a195a86d941cb407196630cdb80
Vanadium5000 <vanadium5000@gmail.com>
2025-11-24 00:26:26 +0000

    librewolf & nix settings

--- modules/nixos/terminal/nix.nix
+++ modules/nixos/terminal/nix.nix
@@ -18,7 +18,30 @@
         "flakes"
       ];
       nix.package = pkgs.lix;
-      nix.settings.trusted-users = [ config.preferences.user.username ];
+      nix.settings = {
+        # Substituters
+        # NOTE: ?priority={num} specificies the priority of the substituter
+        # NOTE: All of this is duplicated both in flake.nix and common/nix.nix
+        # Lower means more priority - cache.nixos.org defaults to 40 priority so it is unchanged
+        extra-substituters = [
+          "https://cache.nixos.org?priority=1"
+          "https://hyprland.cachix.org?priority=2"
+          "https://nix-community.cachix.org?priority=2"
+          "https://cache.soopy.moe?priority=4" # Apple T2
+        ];
+        extra-trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo=" # Apple T2
+        ];
+        builders-use-substitutes = true;
+        trusted-users = [
+          config.preferences.user.username
+          "root"
+          "@wheel"
+        ];
+      };
       programs.nix-ld.enable = true;
       nixpkgs.config = {
         # Disable if you don't want unfree packages
