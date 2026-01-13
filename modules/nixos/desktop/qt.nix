a7df0489cf554e55a83c2dd3c31294b3fd43d9ca
Vanadium5000 <vanadium5000@gmail.com>
2026-01-13 12:24:22 +0000

    oxygen

--- modules/nixos/desktop/qt.nix
+++ modules/nixos/desktop/qt.nix
@@ -1,7 +1,28 @@
 { ... }:
 {
   flake.nixosModules.qt =
-    { pkgs, ... }:
+    {
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    let
+      user = config.preferences.user.username;
+
+      qt5ctConf = lib.generators.toINI {
+        Appearance = {
+          style = "Oxygen";
+          icon_theme = "oxygen";
+          custom_palette = true;
+          color_scheme_path = "${pkgs.kdePackages.qt6ct}/share/qt6ct/colors/darker.conf";
+          standard_dialogs = "default";
+        };
+        Troubleshooting = {
+          ignored_applications = "@Invalid()";
+        };
+      };
+    in
     {
       qt = {
         enable = true;
@@ -11,7 +32,6 @@
       environment.variables = {
         QT_QPA_PLATFORM = "wayland";
         QT_QPA_PLATFORMTHEME = "qt5ct";
-        QT_STYLE_OVERRIDE = "oxygen";
       };
 
       environment.systemPackages = with pkgs; [
@@ -20,5 +40,10 @@
         kdePackages.qt6ct
         libsForQt5.qt5ct
       ];
+
+      hjem.users.${user} = {
+        files.".config/qt5ct/qt5ct.conf".text = qt5ctConf;
+        files.".config/qt6ct/qt6ct.conf".text = qt5ctConf;
+      };
     };
 }
