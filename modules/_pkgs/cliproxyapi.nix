926f5bfb7dcb30ec852630947a8f3c6c6b86594b
Vanadium5000 <vanadium5000@gmail.com>
2026-03-01 00:44:53 +0000

    update cliproxyapi

--- modules/_pkgs/cliproxyapi.nix
+++ modules/_pkgs/cliproxyapi.nix
@@ -7,13 +7,13 @@
 
 unstable.buildGo126Module rec {
   pname = "cliproxyapi";
-  version = "6.8.32";
+  version = "6.8.34";
 
   src = fetchFromGitHub {
     owner = "router-for-me";
     repo = "CLIProxyAPI";
     rev = "v${version}";
-    hash = "sha256-Z0ykwaii0UP7pYqrkrfBXIcg8by1p4xEvvRstioYGFc=";
+    hash = "sha256-Um+J5CzmilUMh87wqTli8JHzrAmuJpcMUSHi1hn/j7A=";
   };
 
   vendorHash = "sha256-3h68+GSEvd7tcJOqTjV2KXBXZFX7AWg3r8K3zZe4DnI=";
